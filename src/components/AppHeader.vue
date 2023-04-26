3bc6a1ebac33daef0471732a185193dff605071a
Luigidefilippo <defilippoluigi65@gmail.com>
2023-04-26 22:20:54 +0200

    add nav bar and first section ( header dont finish

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -9,10 +9,7 @@ export default {
         <div class="container">
             <p>Call us for a Free Quote: 1.800.555.6789</p>
             <ul>
-                <li><font-awesome-icon icon="fa-brands fa-facebook-f" /></li>
-                <li><font-awesome-icon icon="fa-brands fa-twitter" /></li>
-                <li><font-awesome-icon icon="fa-brands fa-instagram" /></li>
-                <li><font-awesome-icon icon="fa-brands fa-youtube" /></li>
+                        <!-- FIX FONT AWESON AND ADD AGAIN SOCIAL ICONS -->
             </ul>
         </div>
     </header>
@@ -34,12 +31,10 @@ header {
         display: flex;
         justify-content: space-between;
         align-items: center;
-        
+
         ul {
             display: flex;
-            justify-content: flex-end;
             gap: 20px;
-            font-size: 16px;
         }
     }
 }
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,14 @@
 <script>
+import mainNavbar from './mainComponents/mainNavbar.vue';
+import mainSection1 from './mainComponents/mainSection1.vue';
+
 export default {
+    components: {
+        mainNavbar,
+        mainSection1,
+    }
 }
+
 </script>
 
 <template>
