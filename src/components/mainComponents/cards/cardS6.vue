53f8f0a7bdb3365b65c4bfd1e995b462227c6124
Luigidefilippo <defilippoluigi65@gmail.com>
2023-04-28 11:25:57 +0200

    Add card testimonial and layout

--- src/components/mainComponents/cards/cardS6.vue
+++ src/components/mainComponents/cards/cardS6.vue
@@ -1,15 +1,42 @@
 <!-- TESTIMONIALS -->
 <script>
-export default{
-
+export default {
+    props: {
+        cardS6Elements: {
+            type: Object,
+            required: true
+        },
+    }
 }
 </script>
 
+
 <template>
+    <li>
+        <img class="testimonials" :src="cardS6Elements.image" alt="">
+        <h5>{{ cardS6Elements.title }}</h5>
+        <p>{{ cardS6Elements.description }}</p>
+    </li>
+
 
 </template>
 
 <style lang="scss">
 @use '../../style/partials/colors' as*;
+
+li {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: center;
+    gap: 40px;
+    line-height: 30px;
+    text-align: center;
+    color: $smallTitle;
+    .testimonials{
+        width: 150px;
+        border-radius: 999px;
+    }
+}
 
 </style>
--- src/components/mainComponents/mainSection6.vue
+++ src/components/mainComponents/mainSection6.vue
@@ -3,9 +3,74 @@
 import cardS6 from './cards/cardS6.vue';
 
 
- export default {
+ export default{
+    data() {
+        return {
+            cardElements: [
+                {
+                    "image": "../../../public/avada-movers-johndoe-final-200x200.jpg",
+                    "title": "John vega ",
+                    "description": "”Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent lectus dolor, aliquet sed maximus et, vestibulum pulvinar leo. Vivamus varius pretium fermentum.”",
+                },
+                {
+                    "image": "../../../public/avada-movers-johndoe-final-200x200.jpg",
+                    "title": "Jane Doe",
+                    "description": "”Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent lectus dolor, aliquet sed maximus et, vestibulum pulvinar leo. Vivamus varius pretium fermentum.”",
+                },
+                {
+                    "image": "../../../public/avada-movers-johndoe-final-200x200.jpg",
+                    "title": "John Smith",
+                    "description": "”Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent lectus dolor, aliquet sed maximus et, vestibulum pulvinar leo. Vivamus varius pretium fermentum.”",
+                },
+            ]
+        }
+    },
+    components: {
+        cardS6,
+    }
 
  }
 
 
 </script>
+
+<template>
+    <section class="s6">
+        <div class="container">
+            <h6>HAPPY CUSTOMERS</h6>
+            <h4>TESTIMONIALS</h4>
+            <ul class="cards">
+                <cardS6 v-for="(element, i) in cardElements" :key="i" :cardS6Elements="element" />
+            </ul>
+            <a href="#" class="button">read more testimonials</a>
+        </div>
+    </section>
+
+</template>
+
+<style lang="scss">
+@use '../style/partials/colors' as*;
+
+
+.s6 {
+    height: 1020px;
+    background-image: url(../../../public/avada-movers-testimonial-background.jpg);
+    background-position: top left;
+    background-size: cover;
+    padding: 0 30px;
+    .container {
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        gap: 20px;
+        .cards {
+            display: flex;
+            justify-content: center;
+            gap: 50px;
+            margin: 40px 0 85px;
+        }
+    }
+}
+</style>
